00873c7e82f5c78b422846564903019145fb14df
Daisy Zhou <daisy@discordapp.com>
2019-06-29 13:37:54 -0700

    remove unnecessary sys.path insert

--- flask_reddit/wsgi.py
+++ flask_reddit/wsgi.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 """
 """
-import sys
-sys.path.insert(0, '/home/lucas/www/reddit.lucasou.com/reddit-env')
-
 from werkzeug.contrib.fixers import ProxyFix # needed for http server proxies
 from werkzeug.debug import DebuggedApplication
 
@@ -11,4 +8,3 @@ from flask_reddit import app # as application
 
 app.wsgi_app = ProxyFix(app.wsgi_app) # needed for http server proxies
 application = DebuggedApplication(app, True)
-
